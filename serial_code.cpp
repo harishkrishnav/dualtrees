eb4980dfb44e0290e7c822931a6afea8af0e6cb5
anurag ghosh <anurag.ghosh@research.iiit.ac.in>
2016-05-04 20:12:43 +0530

    Some expr

--- serial_code.cpp
+++ serial_code.cpp
@@ -16,11 +16,13 @@ int main() {
 	reference_data.load("iris.csv");
 	arma::inplace_trans(query_data);
 	arma::inplace_trans(reference_data);
+	std::cout << reference_data.n_cols << " " <<  reference_data.n_rows<< std::endl;
+	std::cout << query_data.n_cols << " " << reference_data.n_rows << std::endl;
 	AllKNN a(reference_data);
 	arma::Mat<size_t> resulting_neighbors;
 	arma::mat resulting_distances;
 	a.Search(query_data, 1, resulting_neighbors, resulting_distances);
-	for (size_t i = 0; i < resulting_neighbors.n_elem; ++i) {  
+	/*for (size_t i = 0; i < resulting_neighbors.n_elem; ++i) {  
 		std::cout << "pt : " << i << " neigbors : " << resulting_neighbors[i] << " distance : "<< resulting_distances[i] << "\n";
-	}
+	}*/
 }
